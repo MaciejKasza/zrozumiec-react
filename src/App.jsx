346eba024ab4e2b78a7f8ededbf51d647613d21c
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-18 14:39:41 +0100

    Jeden komponent z całą logiką

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const initialState = {
+  isSpoilerShown: false,
+  isWarningShown: true,
+  numberOfLikes: 50,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(initialState);
+
+  function handleLikeButtonClick() {
+    setState((prev) => ({ ...prev, numberOfLikes: prev.numberOfLikes + 1 }));
+  }
+
+  function handleLoveButtonClick() {
+    setState((prev) => ({ ...prev, numberOfLikes: prev.numberOfLikes + 3 }));
+  }
+
+  function handleShowSpoilerClick() {
+    setState((prev) => ({
+      ...prev,
+      isSpoilerShown: true,
+      isWarningShown: false,
+    }));
+  }
+
+  function handleCloseWarningClick() {
+    setState((prev) => ({ ...prev, isWarningShown: false }));
+  }
+
+  console.log("<App> - render");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <h2>Liczba polubień: {state.numberOfLikes}</h2>
+      <button onClick={handleLikeButtonClick}>Lubię to!</button>
+      <button onClick={handleLoveButtonClick}>Kocham to!(+3 like)</button>
+      <h2>Fabuła</h2>
+      {state.isWarningShown && (
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Uwaga! Opis fabuły zawiera spoilery!
+          <button onClick={handleCloseWarningClick}>X</button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
+      {state.isSpoilerShown ? (
+        <p>Vader okazuje się być ojcem Luka.</p>
+      ) : (
+        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
