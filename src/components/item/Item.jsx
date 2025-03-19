bb284187e48862ab33023dffa25d93aa2da7c8d9
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-19 14:27:41 +0100

    module styles

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import styles from "./Item.module.css";
 
 export function Item({ emoji }) {
   const [zoomed, setZoomed] = useState(false);
 
   return (
-    <li>
-      <span>{emoji}</span>
+    <li className={styles.item}>
+      <span className={`${styles.emoji} ${zoomed ? styles.zoomed : ""}`}>
+        {emoji}
+      </span>
       <button
+        className={styles.btn}
         onClick={() => {
           setZoomed((wasZoomed) => !wasZoomed);
         }}
