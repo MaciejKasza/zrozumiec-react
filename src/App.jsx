5baff3e410c4a7415f61ae48ad5dbfba9fc16305
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-24 13:10:41 +0100

    init module 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.module.css";
+import { recipes } from "./data/recipes";
+import { List } from "./components/List/List";
+import { Cookbook } from "./components/Cookbook/Cookbook";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [selectedRecipeId, setSelectedRecipeId] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const selectedRecipe = recipes.find(
+        (recipe) => recipe.id === selectedRecipeId
+    );
+
+    const { name, ingredients, description, img } = selectedRecipe;
+
+    return (
+        <div className={styles.container}>
+            <List
+                recipes={recipes}
+                onSelectRecipe={(id) => setSelectedRecipeId(id)}
+            />
+            <Cookbook
+                name={name}
+                ingredients={ingredients}
+                description={description}
+                img={img}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
