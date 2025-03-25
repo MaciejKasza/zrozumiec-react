d7e3f760a7f06dcdd0ad3549f8e19627b055b830
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-25 13:00:22 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Stocks } from "./components/Stocks/Stocks";
-import { Employees } from "./components/Employees/Employees";
-import { Results } from "./components/Results/Results";
+import "./App.css";
+import { Main } from "./components/Main/Main";
+import { Modal } from "./components/Modal/Modal";
 
 function App() {
     return (
         <>
-            <Stocks />
-            <Employees />
-            <Results />
+            <h1>Witaj w lekcji o React Portal!</h1>
+            <Main />
+            <footer>zrozumiecReact.pl ©</footer>
+            {/* <Modal /> */}
         </>
     );
 }
