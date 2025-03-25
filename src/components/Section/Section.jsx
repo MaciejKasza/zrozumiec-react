be26249353effa812235dba917beef99a1ff6482
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-25 13:24:52 +0100

    createPortal

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./Section.css";
 import { Modal } from "../Modal/Modal";
+import { createPortal } from "react-dom";
 
 export function Section() {
   const [isModalShown, setIsModalShown] = useState(false);
+  const modal = createPortal(
+    <Modal onClose={() => setIsModalShown(false)} />,
+    document.body
+  );
   return (
     <section>
       <h2>Nie czekaj!</h2>
       <h3>Otwórz modal!</h3>
       <button onClick={() => setIsModalShown(true)}>Otwórz</button>
-      {isModalShown && <Modal onClose={() => setIsModalShown(false)} />}
+      {isModalShown && modal}
     </section>
   );
 }
