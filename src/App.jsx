996490d70d742c8f5161338b55ee1bfced98e3dd
Maciej Kasza <kaszamaciej96@gmail.com>
2025-03-25 12:10:58 +0100

    add reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,63 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import styles from "./App.module.css";
 import { Form } from "./components/Form/Form";
 import { TodoItem } from "./components/TodoItem/TodoItem";
 import { getSubheading } from "./utils/getSubheading";
+import { appReducer } from "./reducer/appReducer";
 
 function App() {
-    const [isFormShown, setIsFormShown] = useState(false);
-    const [todos, setTodos] = useState([
-        { name: "Zapłacić rachunki", done: false, id: 1 },
-        { name: "Wyrzucić śmieci", done: true, id: 2 },
-    ]);
+  const [{ todos, isFormShown }, dispatch] = useReducer(appReducer, {
+    todos: [
+      { name: "Zapłacić rachunki", done: false, id: 1 },
+      { name: "Wyrzucić śmieci", done: true, id: 2 },
+    ],
+    isFormShown: false,
+  });
 
-    function addItem(newTodoName) {
-        setTodos((prevTodos) => [
-            ...prevTodos,
-            {
-                name: newTodoName,
-                done: false,
-                id: Math.random(),
-            },
-        ]);
-        setIsFormShown(false);
-    }
+  function addItem(newTodoName) {
+    dispatch({ type: "ADD", newTodoName: newTodoName });
+  }
 
-    function deleteItem(id) {
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-    }
+  function deleteItem(id) {
+    dispatch({ type: "DELETE", id });
+  }
 
-    function finishItem(id) {
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) => {
-                if (todo.id !== id) {
-                    return todo;
-                }
+  function finishItem(id) {
+    dispatch({ type: "FINISH", id });
+  }
 
-                return {
-                    ...todo,
-                    done: true,
-                };
-            })
-        );
-    }
-
-    return (
-        <div className={styles.container}>
-            <header className={styles.header}>
-                <div>
-                    <h1>Do zrobienia</h1>
-                    <h2>{getSubheading(todos.length)}</h2>
-                </div>
-                {!isFormShown && (
-                    <button
-                        onClick={() => setIsFormShown(true)}
-                        className={styles.button}
-                    >
-                        +
-                    </button>
-                )}
-            </header>
-            {isFormShown && (
-                <Form onFormSubmit={(newTodoName) => addItem(newTodoName)} />
-            )}
-            <ul>
-                {todos.map(({ id, name, done }) => (
-                    <TodoItem
-                        key={id}
-                        name={name}
-                        done={done}
-                        onDeleteButtonClick={() => deleteItem(id)}
-                        onDoneButtonClick={() => finishItem(id)}
-                    />
-                ))}
-            </ul>
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div>
+          <h1>Do zrobienia</h1>
+          <h2>{getSubheading(todos.length)}</h2>
         </div>
-    );
+        {!isFormShown && (
+          <button
+            onClick={() => dispatch({ type: "OPEN_FORM" })}
+            className={styles.button}
+          >
+            +
+          </button>
+        )}
+      </header>
+      {isFormShown && (
+        <Form onFormSubmit={(newTodoName) => addItem(newTodoName)} />
+      )}
+      <ul>
+        {todos.map(({ id, name, done }) => (
+          <TodoItem
+            key={id}
+            name={name}
+            done={done}
+            onDeleteButtonClick={() => deleteItem(id)}
+            onDoneButtonClick={() => finishItem(id)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
